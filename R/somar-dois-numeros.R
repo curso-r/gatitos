b559fff4580ea467f8419a85f3f8946bccac1e31
Beatriz Milz <beatriz.milz@hotmail.com>
2021-09-18 15:57:08 -0300

    Initial commit

---
+++ R/somar-dois-numeros.R
@@ -0,0 +1,12 @@
+#' soma dois numeros
+#'
+#' @param x um numero
+#' @param y outro numero
+#'
+#' @return um numero
+#' @export
+#'
+#' @examples somar_dois_numeros(1, 2)
+somar_dois_numeros <- function(x, y){
+  x + y
+}
